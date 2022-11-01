18e9e8b6a94ceb06c56ffc6032e3e2ce33b4f2a2
Tomáš Kudělka <tomas@appsdevteam.com>
2022-11-01 18:24:54 +0100

    Update DoctrineAuthenticator.php

--- src/DoctrineAuthenticator.php
+++ src/DoctrineAuthenticator.php
@@ -8,6 +8,8 @@ use Nette\Security\Authenticator;
 use Nette\Security\IdentityHandler;
 use Nette\Security\IIdentity;
 use Nette\Security\SimpleIdentity;
+use Nette\Security\User;
+use Nette\Security\UserStorage;
 
 abstract class DoctrineAuthenticator implements Authenticator, IdentityHandler
 {
@@ -15,9 +17,11 @@ abstract class DoctrineAuthenticator implements Authenticator, IdentityHandler
 	 * @throws Exception
 	 */
 	public function __construct(
+		protected readonly string $expiration,
 		protected readonly string $className,
 		protected readonly string $fieldName,
-		protected readonly EntityManagerInterface $em
+		protected readonly EntityManagerInterface $em,
+		protected readonly UserStorage $userStorage
 	) {
 		if (! is_a($className, DoctrineAuthenticatorSession::class, true)) {
 			throw new Exception("Class '$className' must implements 'DoctrineAuthenticatorSession' interface!");
@@ -41,6 +45,11 @@ abstract class DoctrineAuthenticator implements Authenticator, IdentityHandler
 	{
 		/** @var DoctrineAuthenticatorSession $entity */
 		$entity = $this->em->getRepository($this->className)->findOneBy([$this->fieldName => $identity->getId()]);
+
+		if ($entity) {
+			$this->userStorage->setExpiration($this->expiration, false);
+			$this->userStorage->saveAuthentication($identity);
+		}
 
 		return $entity?->getAuthEntity();
 	}
